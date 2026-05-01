ffb0adb776ad61792ab732ad992919a88369487f
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-05-01 08:34:53 +0600

    from desginen

--- src/app/deltes/[id]/page.jsx
+++ src/app/deltes/[id]/page.jsx
@@ -1,9 +1,29 @@
+import Image from 'next/image';
 import React from 'react';
 
-const DetelsPage = () => {
+const DetelsPage = async({ params }) => {
+    const {id} = await params;
+    const res = await fetch('http://localhost:3000/data.json',{
+    cache: 'no-store'
+  })
+  const data = await res.json();
+  const det = data.find(dtr=> dtr.id == id);
+  console.log(det)
     return (
-        <div>
-            afdfafasfdf
+        <div className='mx-auto container flex mt-8 gap-4 bg-white p-4 shadow border rounded-sm'>
+           <div>
+            <Image className='rounded-md' src={det.image_url} alt='book image' width={1500} height={1000}></Image>
+            </div>
+            <div>
+                 <h2 className='text-4xl font-bold'>{det.title}</h2>
+                 <p className='opacity-50' >{det.description}</p>
+                  <div className='flex gap-8 mt-4'>
+                          <p className='bg-green-300 rounded-sm px-2 text-green-600 font-bold'>Author: {det.author}</p>
+                          <p className='bg-red-300 rounded-sm px-1 text-red-600 font-bold'>Quantity: {det.available_quantity}</p>
+                         
+                        </div>
+                        <button className='mt-4 bg-blue-600 text-white px-32 rounded-md font-bold text-xl'>Borrow This Book</button>
+            </div>
         </div>
     );
 };
---
+++ src/app/sinup/pag.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingUpPage = () => {
+    return (
+        <div>
+        
+        </div>
+    );
+};
+
+export default SingUpPage;
